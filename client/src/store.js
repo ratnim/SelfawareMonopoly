20c30fb015775783a8718742280cb5194f77b186
paszin <pascalslogin@gmail.com>
2018-05-25 14:35:55 +0200

    save state to local storage #47

--- client/src/store.js
+++ client/src/store.js
@@ -7,9 +7,20 @@ import {
 } from 'vue-authenticate'
 import axios from 'axios';
 
+import VuexPersist from 'vuex-persist';
+
 Vue.use(Vuex)
 
 Vue.use(VueAxios, axios)
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  // reducer: state => state,
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+})
 
 const vueAuth = new VueAuthenticate(Vue.prototype.$http, {
   baseUrl: window.origin,
@@ -47,6 +58,7 @@ const vueAuth = new VueAuthenticate(Vue.prototype.$http, {
 })
 
 export default new Vuex.Store({
+  plugins: [vuexLocalStorage.plugin],
   state: {
     nickname : '',
     isAuthenticated: false,
