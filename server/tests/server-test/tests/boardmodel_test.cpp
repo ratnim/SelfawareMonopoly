8d29470064049c481d869d779b62f91195b5f1b8
martin fischer <martin.fis@gmx.net>
2018-07-27 13:45:51 +0200

    add start type check into boardmodel test

--- server/tests/server-test/tests/boardmodel_test.cpp
+++ server/tests/server-test/tests/boardmodel_test.cpp
@@ -7,7 +7,6 @@ class BoardModelTest : public ::testing::Test
 public:
 };
 
-
 TEST_F(BoardModelTest, board_dir_not_set)
 {
     auto& instance = BoardModel::instance();
@@ -34,5 +33,6 @@ TEST_F(BoardModelTest, load_board)
     auto& instance = BoardModel::instance();
     auto board = std::move(instance.new_board("berlin.json"));
 
-	EXPECT_EQ(40, board.size());
+    EXPECT_EQ(40, board.size());
+    EXPECT_EQ(FieldType::start, board[0]->type());
 }
