9392e975cd6197f5f971dc773d3779493d3abd38
martin fischer <martin.fis@gmx.net>
2018-08-16 17:25:07 +0200

    extends game board request test

--- server/tests/server-test/tests/gameroute_test.cpp
+++ server/tests/server-test/tests/gameroute_test.cpp
@@ -8,6 +8,7 @@
 
 #include <models/accountmodel.h>
 #include <models/gamemodel.h>
+#include <models/boardmodel.h>
 #include <network/request.h>
 
 namespace
@@ -18,7 +19,7 @@ QWebSocket dummy;
 TEST(GameRouteTest, request_board)
 {
     auto session = AccountModel::instance().createSession("request_board_tester");
-    auto gameId = GameModel::instance().createGame("request_board_test_game");
+    auto gameId = GameModel::instance().createGame("request_board_game");
 	Request request("game", session, gameId);
 
     QWebSocket socket;
@@ -35,4 +36,29 @@ TEST(GameRouteTest, request_board)
 	auto json = route->toJson(socket_spy.at(0).at(0).toString());
 
 	EXPECT_NE(json["data"].toObject().end(), json["data"].toObject().find("fields"));
+}
+
+TEST(GameRouteTest, request_non_empty_board)
+{
+	BoardModel::setBoardDir("./../../../assets/boards");
+    auto session = AccountModel::instance().createSession("request_non_empty_board_tester");
+    auto gameId = GameModel::instance().createGame("request_non_empty_board_game", "berlin.json");
+    Request request("game", session, gameId);
+
+    QWebSocket socket;
+    auto route = new GameRoute(&socket, request);
+
+    QSignalSpy socket_spy(route, &Watcher::send);
+
+    QJsonObject in({ { "request", "game_board" } });
+
+    route->incommingMessage(QJsonDocument(in).toJson());
+
+    ASSERT_TRUE(socket_spy.size() || socket_spy.wait());
+
+	auto msg = socket_spy.at(0).at(0).toString().toStdString();
+    auto json = route->toJson(socket_spy.at(0).at(0).toString());
+    auto fields = json["data"].toObject()["fields"].toArray();
+
+    EXPECT_EQ(40, fields.size());
 }
