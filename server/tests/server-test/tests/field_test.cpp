3d2a34962f5d70bb16f0a4d0cd19c67904591846
martin fischer <martin.fis@gmx.net>
2018-08-16 20:22:00 +0200

    adds exceptions to non existing name and type

--- server/tests/server-test/tests/field_test.cpp
+++ server/tests/server-test/tests/field_test.cpp
@@ -12,3 +12,17 @@ TEST(FieldTest, get_description)
     EXPECT_EQ("Test", description["name"].toString().toStdString());
     EXPECT_EQ("start", description["type"].toString().toStdString());
 }
+
+TEST(FieldTypeStringConverterTest, type_to_name)
+{
+    EXPECT_EQ(FieldType::jail, FieldTypeStringConverter::typeByName("jail"));
+	EXPECT_EQ(FieldType::go_to_jail, FieldTypeStringConverter::typeByName("go_to_jail"));
+    EXPECT_ANY_THROW(FieldTypeStringConverter::typeByName("not_there"));
+}
+
+TEST(FieldTypeStringConverterTest, name_to_type)
+{
+    EXPECT_EQ("jail", FieldTypeStringConverter::nameByType(FieldType::jail));
+    EXPECT_EQ("go_to_jail", FieldTypeStringConverter::nameByType(FieldType::go_to_jail));
+    EXPECT_ANY_THROW(FieldTypeStringConverter::nameByType(static_cast<FieldType>(-1)));
+}
