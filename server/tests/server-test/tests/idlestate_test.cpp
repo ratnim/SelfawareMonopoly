be5a059da3bb730d6448c401f81296e94b5d8316
martin fischer <martin.fis@gmx.net>
2018-08-26 15:22:35 +0200

    add idle state tests

---
+++ server/tests/server-test/tests/idlestate_test.cpp
@@ -0,0 +1,76 @@
+#include <gmock/gmock.h>
+
+#include <QSignalSpy>
+
+#include <game/game.h>
+
+#include <game/turn/idlestate.h>
+#include <game/turn/rollstate.h>
+
+#include <utils/exception.h>
+
+#include <test_utils/helpers.h>
+
+namespace
+{
+const QString player_1("Heinz");
+const QString player_2("Gertrude");
+}
+
+TEST(IdleStateTest, end_turn)
+{
+    Game game;
+    game.players() = RingBuffer<Player>(std::vector<Player>{ { player_1, player_2 } });
+    game.stateChange<IdleState>();
+
+    QSignalSpy turnChange_spy(&game, &Game::onTurnChange);
+
+    EXPECT_EQ(player_1, game.currentPlayer().name());
+    game.endTurn(player_1);
+
+    EXPECT_EQ(turnChange_spy.size(), 1);
+}
+
+
+TEST(IdleStateTest, state_transfer_to_roll)
+{
+    Game game;
+    game.players() = RingBuffer<Player>(std::vector<Player>{ { player_1, player_2 } });
+    game.stateChange<IdleState>();
+
+    EXPECT_EQ(player_1, game.currentPlayer().name());
+    game.endTurn(player_1);
+
+    EXPECT_NE(nullptr, dynamic_cast<RollState*>(game.state()));
+    EXPECT_EQ(player_2, game.currentPlayer().name());
+}
+
+TEST(IdleStateTest, possible_requests)
+{
+    Game game;
+    game.players() = RingBuffer<Player>(std::vector<Player>{ { player_1, player_2 } });
+    game.stateChange<IdleState>();
+    QSignalSpy request_spy(&game, &Game::onPossibleRequests);
+
+    game.possibleRequests(player_1);
+
+    EXPECT_EQ(player_1, game.currentPlayer().name());
+    EXPECT_EQ(player_1, request_spy.last().at(0).toString());
+    EXPECT_TRUE(containsRequest(request_spy.last().at(1).toJsonArray(), "end_turn"));
+
+    game.possibleRequests(player_2);
+
+    EXPECT_EQ(request_spy.last().at(0).toString(), player_2);
+    EXPECT_FALSE(containsRequest(request_spy.last().at(1).toJsonArray(), "end_turn"));
+}
+
+TEST(IdleStateTest, update_possible_actions)
+{
+    Game game;
+    game.players() = RingBuffer<Player>(std::vector<Player>{ { player_1, player_2 } });
+    QSignalSpy request_spy(&game, &Game::onPossibleRequests);
+
+    game.stateChange<IdleState>();
+
+    EXPECT_EQ(2, request_spy.size());
+}
