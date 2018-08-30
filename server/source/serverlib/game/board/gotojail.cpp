e6d659fb00a81e560fcea18cae36adfd6e79b546
martin fischer <martin.fis@gmx.net>
2018-08-30 10:07:09 +0200

    implements go to jail triggered by move on

--- server/source/serverlib/game/board/gotojail.cpp
+++ server/source/serverlib/game/board/gotojail.cpp
@@ -1,6 +1,17 @@
 #include "gotojail.h"
 
+#include <game/game.h>
+
 GoToJail::GoToJail(const QString& name) : Field(name, FieldType::go_to_jail)
 {
+}
 
+bool GoToJail::moveOnTriggersStateChange(const QString& name) const
+{
+    return true;
+}
+
+void GoToJail::moveOn(const QString& playerName, Game* game)
+{
+    game->doJailCurrentPlayer();
 }
--- server/source/serverlib/game/board/gotojail.h
+++ server/source/serverlib/game/board/gotojail.h
@@ -6,4 +6,7 @@ class GoToJail : public Field
 {
 public:
     GoToJail(const QString& name);
+
+	bool moveOnTriggersStateChange(const QString& playerName) const override;
+    void moveOn(const QString& playerName, Game* game) override;
 };
