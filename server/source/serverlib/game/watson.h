5407bbfbb1647ecf54eab3821c6f4fef7b9a69e8
martin fischer <martin.fis@gmx.net>
2018-09-08 10:44:40 +0200

    implements watson coins

--- server/source/serverlib/game/watson.h
+++ server/source/serverlib/game/watson.h
@@ -20,14 +20,24 @@ public:
 	void doHarmCurrentPlayer();
     void doManipulateNextRoll(const QString& playerName, Dices dices);
     void doManipulateNextRoll(const QString& playerName, int x, int y);
-    void doAddWatsonCoins(const QString& playerName, int amount);
+    void doAddWatsonCoins(const QString& playerName, int coins);
+	void doUseCoinsForMoney(const QString&, int money);
 
     Dices getManipulatedDices(const QString& playerName);
+    int availableMoneyForCoins(const QString& playerName);
 
 	bool diceAreManipulated(const QString& playerName) const;
     bool currentPlayerRequest(const QString& playerName) const;	
 
+    static int coinsToMoney(int coins);
+    static int coinsForMoneyNeeded(int money);
+
 protected:
+    void createCoinAccountIfNotExisting(const QString& playerName);
+	
+	static const int course = 50;
+
     Game* m_game;
     std::map<QString, Dices> m_manipulatedDices;
+    std::map<QString, int> m_coins;
 };
--- server/tests/server-test/tests/watson_test.cpp
+++ server/tests/server-test/tests/watson_test.cpp
@@ -6,6 +6,8 @@
 #include <game/turn/movestate.h>
 #include <game/watson.h>
 
+#include <utils/exception.h>
+
 #include <test_utils/helpers.h>
 
 namespace
@@ -23,11 +25,11 @@ TEST(WatsonTest, manipulate_dices)
 
     QSignalSpy roll_spy(&game, &Game::onRollDice);
 
-    game.watson().doManipulateNextRoll(player_1, {6, 6});
+    game.watson().doManipulateNextRoll(player_1, { 6, 6 });
 
     game.requestRollDice(player_1);
 
-	EXPECT_EQ(6, roll_spy.last().at(1).toInt());
+    EXPECT_EQ(6, roll_spy.last().at(1).toInt());
     EXPECT_EQ(6, roll_spy.last().at(2).toInt());
 }
 
@@ -56,7 +58,7 @@ TEST(WatsonTest, harm_player_on_request_not_overrides)
 
     QSignalSpy roll_spy(&game, &Game::onRollDice);
 
-	game.watson().doManipulateNextRoll(player_1, 1, 2);
+    game.watson().doManipulateNextRoll(player_1, 1, 2);
 
     game.watson().requestAddClick(player_2, "test_add");
 
@@ -74,7 +76,7 @@ TEST(WatsonTest, scan_gmail)
 
     QSignalSpy roll_spy(&game, &Game::onRollDice);
 
-    game.watson().requestScannedGMailAccount(player_1, {1, 2});
+    game.watson().requestScannedGMailAccount(player_1, { 1, 2 });
 
     game.requestRollDice(player_1);
 
@@ -92,9 +94,52 @@ TEST(WatsonTest, scan_gmail_overrides)
 
     game.watson().doManipulateNextRoll(player_1, 1, 2);
 
-    game.watson().requestScannedGMailAccount(player_1, {2, 3});
+    game.watson().requestScannedGMailAccount(player_1, { 2, 3 });
 
     game.requestRollDice(player_1);
 
     EXPECT_EQ(5, roll_spy.last().at(1).toInt() + roll_spy.last().at(2).toInt());
+}
+
+TEST(WatsonTest, coins_to_money)
+{
+    EXPECT_EQ(50, Watson::coinsToMoney(1));
+    EXPECT_EQ(150, Watson::coinsToMoney(3));
+}
+
+TEST(WatsonTest, coins_for_money_needed)
+{
+    EXPECT_EQ(1, Watson::coinsForMoneyNeeded(1));
+    EXPECT_EQ(1, Watson::coinsForMoneyNeeded(49));
+    EXPECT_EQ(1, Watson::coinsForMoneyNeeded(50));
+    EXPECT_EQ(2, Watson::coinsForMoneyNeeded(51));
+}
+
+TEST(WatsonTest, money_for_coins_available)
+{
+    Game game;
+
+    EXPECT_EQ(0, game.watson().availableMoneyForCoins(player_1));
+    game.watson().doAddWatsonCoins(player_1, 1);
+    EXPECT_EQ(50, game.watson().availableMoneyForCoins(player_1));
+
+    game.watson().doAddWatsonCoins(player_2, 3);
+    EXPECT_EQ(150, game.watson().availableMoneyForCoins(player_2));
+}
+
+TEST(WatsonTest, use_money_for_coins)
+{
+    Game game;
+
+    game.watson().doAddWatsonCoins(player_1, 4);
+    game.watson().doUseCoinsForMoney(player_1, 50);
+
+    EXPECT_EQ(150, game.watson().availableMoneyForCoins(player_1));
+
+    game.watson().doUseCoinsForMoney(player_1, 75);
+
+    EXPECT_EQ(50, game.watson().availableMoneyForCoins(player_1));
+
+    EXPECT_THROW(game.watson().doUseCoinsForMoney(player_1, 100), Exception);
+    EXPECT_EQ(50, game.watson().availableMoneyForCoins(player_1));
 }
